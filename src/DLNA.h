98cccf03d9f55f7c14767b0f26f91864e64c60ff
pschatzmann <phil.schatzmann@gmail.com>
2024-05-13 09:48:51 +0200

    Desktop compile

--- src/DLNA.h
+++ src/DLNA.h
@@ -2,6 +2,12 @@
 #include "dlna/DLNADevice.h"
 #include "dlna/devices/MediaRenderer/MediaRenderer.h"
 #include "dlna/udp/UDPService.h"
-#include "dlna/udp/UDPAsyncService.h"
+#ifndef IS_DESKTOP
+#  include "dlna/udp/UDPAsyncService.h"
+#endif
 
 using namespace tiny_dlna;
+
+#ifdef IS_DESKTOP
+using UDPAsyncService = UDPService;
+#endif
