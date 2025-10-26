9ca7ac8714ac5f395858e5a7095949e789882721
pschatzmann <phil.schatzmann@gmail.com>
2025-10-26 22:13:38 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@b60cc143ae50d713448e39fac6d5045b7b3a293c 🚀

--- search/enumvalues_5.js
+++ search/enumvalues_5.js
@@ -4,11 +4,12 @@ var searchData=
   ['t_5fdelete_1',['T_DELETE',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a2c2f34b372dafc24218c63d87a4162bb',1,'tiny_dlna']]],
   ['t_5fget_2',['T_GET',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a97fc3ce47a74a4e9ed375738b0b77c17',1,'tiny_dlna']]],
   ['t_5fhead_3',['T_HEAD',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a8855246fa66f6be3476cf1cbfa203a27',1,'tiny_dlna']]],
-  ['t_5foptions_4',['T_OPTIONS',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a439b85af4776b7be46ca12db98031c03',1,'tiny_dlna']]],
-  ['t_5fpatch_5',['T_PATCH',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247acaf97fba9939f1bea8cd1141de6b7666',1,'tiny_dlna']]],
-  ['t_5fpost_6',['T_POST',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247ac156bd726a28ae5da1401917117bea4a',1,'tiny_dlna']]],
-  ['t_5fput_7',['T_PUT',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a9945be3cffff88bf64699d564df76128',1,'tiny_dlna']]],
-  ['t_5fsubscribe_8',['T_SUBSCRIBE',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a78d7f90932c20309de58ac2111387323',1,'tiny_dlna']]],
-  ['t_5ftrace_9',['T_TRACE',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a26535a2ef4d0916cd3b9e0d0716183a6',1,'tiny_dlna']]],
-  ['t_5fundefined_10',['T_UNDEFINED',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a38bf86d84d310d359e8de997cf53b32f',1,'tiny_dlna']]]
+  ['t_5fnotify_4',['T_NOTIFY',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247ad90b59c95e2a57ad2969a295ee3bcff2',1,'tiny_dlna']]],
+  ['t_5foptions_5',['T_OPTIONS',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a439b85af4776b7be46ca12db98031c03',1,'tiny_dlna']]],
+  ['t_5fpatch_6',['T_PATCH',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247acaf97fba9939f1bea8cd1141de6b7666',1,'tiny_dlna']]],
+  ['t_5fpost_7',['T_POST',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247ac156bd726a28ae5da1401917117bea4a',1,'tiny_dlna']]],
+  ['t_5fput_8',['T_PUT',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a9945be3cffff88bf64699d564df76128',1,'tiny_dlna']]],
+  ['t_5fsubscribe_9',['T_SUBSCRIBE',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a78d7f90932c20309de58ac2111387323',1,'tiny_dlna']]],
+  ['t_5ftrace_10',['T_TRACE',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a26535a2ef4d0916cd3b9e0d0716183a6',1,'tiny_dlna']]],
+  ['t_5fundefined_11',['T_UNDEFINED',['../namespacetiny__dlna.html#a8b30900e538135f563c83e48319e5247a38bf86d84d310d359e8de997cf53b32f',1,'tiny_dlna']]]
 ];
--- search/functions_11.js
+++ search/functions_11.js
@@ -6,9 +6,8 @@ var searchData=
   ['toint_3',['toInt',['../classtiny__dlna_1_1StrView.html#a5d0106fca19f46ae400a0e1c57f5ccf4',1,'tiny_dlna::StrView']]],
   ['tolong_4',['toLong',['../classtiny__dlna_1_1StrView.html#a1f9b02a605f1c7d62842d5c5ae5b03f8',1,'tiny_dlna::StrView']]],
   ['tolowercase_5',['toLowerCase',['../classtiny__dlna_1_1StrView.html#aaf82fe261dd3bf9f1a6464c3ef7bd5f4',1,'tiny_dlna::StrView']]],
-  ['tostr_6',['toStr',['../classtiny__dlna_1_1DLNAControlPoint.html#a385e8e719df4cfb663779b2d1761c6f5',1,'tiny_dlna::DLNAControlPoint::toStr(const char *str, const char *empty=&quot;(null)&quot;)'],['../classtiny__dlna_1_1DLNAControlPoint.html#ad7e3d3330c28ab99bdec49fb5511f395',1,'tiny_dlna::DLNAControlPoint::toStr(Str &amp;str, const char *empty=&quot;(null)&quot;)']]],
-  ['tostring_7',['toString',['../structtiny__dlna_1_1IPAddressAndPort.html#a03dafeafa067fa880a6a1d05d8c748e1',1,'tiny_dlna::IPAddressAndPort']]],
-  ['touppercase_8',['toUpperCase',['../classtiny__dlna_1_1StrView.html#afddb2b1aff609ff143a4d4bd99d38c9d',1,'tiny_dlna::StrView']]],
-  ['transportcontrolcb_9',['transportControlCB',['../classtiny__dlna_1_1MediaRenderer.html#a424b5fd52529241b220c7e9052710c3b',1,'tiny_dlna::MediaRenderer']]],
-  ['trim_10',['trim',['../classtiny__dlna_1_1StrView.html#a8ece7148c765b8a590ec997715335c04',1,'tiny_dlna::StrView']]]
+  ['tostring_6',['toString',['../structtiny__dlna_1_1IPAddressAndPort.html#a03dafeafa067fa880a6a1d05d8c748e1',1,'tiny_dlna::IPAddressAndPort']]],
+  ['touppercase_7',['toUpperCase',['../classtiny__dlna_1_1StrView.html#afddb2b1aff609ff143a4d4bd99d38c9d',1,'tiny_dlna::StrView']]],
+  ['transportcontrolcb_8',['transportControlCB',['../classtiny__dlna_1_1MediaRenderer.html#a424b5fd52529241b220c7e9052710c3b',1,'tiny_dlna::MediaRenderer']]],
+  ['trim_9',['trim',['../classtiny__dlna_1_1StrView.html#a8ece7148c765b8a590ec997715335c04',1,'tiny_dlna::StrView']]]
 ];
