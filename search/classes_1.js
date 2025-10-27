f4b0907652d492753bd745c5204840b6d78b1420
pschatzmann <phil.schatzmann@gmail.com>
2025-10-27 16:17:59 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@fbc8bc50f8256e917f18181504cb02ff1c9e24d1 🚀

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['cbref_0',['CBRef',['../structtiny__dlna_1_1XMLParserPrint_1_1CBRef.html',1,'tiny_dlna::XMLParserPrint']]],
-  ['controlpointmediarenderer_1',['ControlPointMediaRenderer',['../classtiny__dlna_1_1ControlPointMediaRenderer.html',1,'tiny_dlna']]],
-  ['controlpointmediaserver_2',['ControlPointMediaServer',['../classtiny__dlna_1_1ControlPointMediaServer.html',1,'tiny_dlna']]]
+  ['chunkprint_1',['ChunkPrint',['../classtiny__dlna_1_1ChunkPrint.html',1,'tiny_dlna']]],
+  ['controlpointmediarenderer_2',['ControlPointMediaRenderer',['../classtiny__dlna_1_1ControlPointMediaRenderer.html',1,'tiny_dlna']]],
+  ['controlpointmediaserver_3',['ControlPointMediaServer',['../classtiny__dlna_1_1ControlPointMediaServer.html',1,'tiny_dlna']]]
 ];
