afb91fa15bdb361eb0001186b60734e52157d723
pschatzmann <phil.schatzmann@gmail.com>
2025-10-31 21:43:39 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@a29d080a9bd1a2534950e497b1ebe989490706f3 🚀

--- search/variables_a.js
+++ search/variables_a.js
@@ -5,13 +5,16 @@ var searchData=
   ['last_5fattributes_2',['last_attributes',['../classtiny__dlna_1_1XMLParser.html#a0181e2e4bf8110abf69db577b00f4a32',1,'tiny_dlna::XMLParser']]],
   ['last_5fevent_5fnotify_5fms_3',['last_event_notify_ms',['../classtiny__dlna_1_1SubscriptionMgrControlPoint.html#a19faa4c6793a8ed88559a8e9b5856484',1,'tiny_dlna::SubscriptionMgrControlPoint']]],
   ['last_5freply_4',['last_reply',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html#a868a1d3e694d0d082bf2334a0b8f29f9',1,'tiny_dlna::DLNAControlPointMediaRenderer::last_reply()'],['../classtiny__dlna_1_1DLNAControlPointMediaServer.html#adee5db73ee1ebcfe06a951edd1a36597',1,'tiny_dlna::DLNAControlPointMediaServer::last_reply()']]],
-  ['len_5',['len',['../structtiny__dlna_1_1XMLParserPrint_1_1CBRef.html#a0d9526b4399cf64825e2b0d12240a261',1,'tiny_dlna::XMLParserPrint::CBRef::len()'],['../classtiny__dlna_1_1Vector.html#abc61d2836d97b5a611f49a17167402cb',1,'tiny_dlna::Vector::len()'],['../classtiny__dlna_1_1StrView.html#a84766049d01621d878a6aafe1c7c35f4',1,'tiny_dlna::StrView::len()']]],
+  ['len_5',['len',['../classtiny__dlna_1_1StrView.html#a84766049d01621d878a6aafe1c7c35f4',1,'tiny_dlna::StrView::len()'],['../classtiny__dlna_1_1Vector.html#abc61d2836d97b5a611f49a17167402cb',1,'tiny_dlna::Vector::len()'],['../structtiny__dlna_1_1XMLParserPrint_1_1CBRef.html#a0d9526b4399cf64825e2b0d12240a261',1,'tiny_dlna::XMLParserPrint::CBRef::len()']]],
   ['lines_6',['lines',['../classtiny__dlna_1_1HttpHeader.html#a0303dd1387028e16f3c48b312e28a44c',1,'tiny_dlna::HttpHeader']]],
   ['local_5fhost_7',['local_host',['../classtiny__dlna_1_1HttpServer.html#a47eeb593260afbfb1e03587f010289bd',1,'tiny_dlna::HttpServer']]],
-  ['local_5furl_8',['local_url',['../classtiny__dlna_1_1DLNAControlPoint.html#a3a1bb5777ddf5319020bdea9a92c14a7',1,'tiny_dlna::DLNAControlPoint']]],
-  ['localhost_9',['localhost',['../classtiny__dlna_1_1DLNADeviceInfo.html#a28d8ce438c8338faf5ad279f2f26d9b4',1,'tiny_dlna::DLNADeviceInfo']]],
-  ['location_10',['location',['../classtiny__dlna_1_1MSearchReplyCP.html#ac2c66bea457ce9b9337d8e5b27e53971',1,'tiny_dlna::MSearchReplyCP']]],
-  ['location_11',['LOCATION',['../namespacetiny__dlna.html#afce8f59f39df8a9aed9f14e4c56dcd84',1,'tiny_dlna']]],
-  ['log_5flevel_12',['log_level',['../classtiny__dlna_1_1LoggerClass.html#a183ab881f8dc3feab082a38d7d301704',1,'tiny_dlna::LoggerClass']]],
-  ['log_5fstream_5fptr_13',['log_stream_ptr',['../classtiny__dlna_1_1LoggerClass.html#a704408410ecd0260af3659b212a10d8b',1,'tiny_dlna::LoggerClass']]]
+  ['local_5fmute_8',['local_mute',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html#a1bad194a60906ce811e962e15f8e01c2',1,'tiny_dlna::DLNAControlPointMediaRenderer']]],
+  ['local_5ftransport_5fstate_9',['local_transport_state',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html#a100cc3f9b4874e8aa5595608d2214a3c',1,'tiny_dlna::DLNAControlPointMediaRenderer']]],
+  ['local_5furl_10',['local_url',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html#a831b7a95f5ba360d8d3b39348851291f',1,'tiny_dlna::DLNAControlPointMediaRenderer::local_url()'],['../classtiny__dlna_1_1DLNAControlPoint.html#a3a1bb5777ddf5319020bdea9a92c14a7',1,'tiny_dlna::DLNAControlPoint::local_url()']]],
+  ['local_5fvolume_11',['local_volume',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html#a2be36580cbd51a2e7e39bf795c853c90',1,'tiny_dlna::DLNAControlPointMediaRenderer']]],
+  ['localhost_12',['localhost',['../classtiny__dlna_1_1DLNADeviceInfo.html#a28d8ce438c8338faf5ad279f2f26d9b4',1,'tiny_dlna::DLNADeviceInfo']]],
+  ['location_13',['location',['../classtiny__dlna_1_1MSearchReplyCP.html#ac2c66bea457ce9b9337d8e5b27e53971',1,'tiny_dlna::MSearchReplyCP']]],
+  ['location_14',['LOCATION',['../namespacetiny__dlna.html#afce8f59f39df8a9aed9f14e4c56dcd84',1,'tiny_dlna']]],
+  ['log_5flevel_15',['log_level',['../classtiny__dlna_1_1LoggerClass.html#a183ab881f8dc3feab082a38d7d301704',1,'tiny_dlna::LoggerClass']]],
+  ['log_5fstream_5fptr_16',['log_stream_ptr',['../classtiny__dlna_1_1LoggerClass.html#a704408410ecd0260af3659b212a10d8b',1,'tiny_dlna::LoggerClass']]]
 ];
