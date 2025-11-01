2aeda37e179eac7b4677efb1e11ef0e2db1cb997
pschatzmann <phil.schatzmann@gmail.com>
2025-11-01 05:27:06 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@7e55504e8d24da833677a19d03cceb890653a5df 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,14 +1,13 @@
 var searchData=
 [
-  ['dlnacontext_0',['DLNAContext',['../classtiny__dlna_1_1DLNAContext.html',1,'tiny_dlna']]],
-  ['dlnacontrolpoint_1',['DLNAControlPoint',['../classtiny__dlna_1_1DLNAControlPoint.html',1,'tiny_dlna']]],
-  ['dlnacontrolpointmediarenderer_2',['DLNAControlPointMediaRenderer',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html',1,'tiny_dlna']]],
-  ['dlnacontrolpointmediaserver_3',['DLNAControlPointMediaServer',['../classtiny__dlna_1_1DLNAControlPointMediaServer.html',1,'tiny_dlna']]],
-  ['dlnacontrolpointrequestparser_4',['DLNAControlPointRequestParser',['../classtiny__dlna_1_1DLNAControlPointRequestParser.html',1,'tiny_dlna']]],
-  ['dlnadevice_5',['DLNADevice',['../classtiny__dlna_1_1DLNADevice.html',1,'tiny_dlna']]],
-  ['dlnadeviceinfo_6',['DLNADeviceInfo',['../classtiny__dlna_1_1DLNADeviceInfo.html',1,'tiny_dlna']]],
-  ['dlnadevicerequestparser_7',['DLNADeviceRequestParser',['../classtiny__dlna_1_1DLNADeviceRequestParser.html',1,'tiny_dlna']]],
-  ['dlnamediarenderer_8',['DLNAMediaRenderer',['../classtiny__dlna_1_1DLNAMediaRenderer.html',1,'tiny_dlna']]],
-  ['dlnamediaserver_9',['DLNAMediaServer',['../classtiny__dlna_1_1DLNAMediaServer.html',1,'tiny_dlna']]],
-  ['dlnaserviceinfo_10',['DLNAServiceInfo',['../classtiny__dlna_1_1DLNAServiceInfo.html',1,'tiny_dlna']]]
+  ['dlnacontrolpoint_0',['DLNAControlPoint',['../classtiny__dlna_1_1DLNAControlPoint.html',1,'tiny_dlna']]],
+  ['dlnacontrolpointmediarenderer_1',['DLNAControlPointMediaRenderer',['../classtiny__dlna_1_1DLNAControlPointMediaRenderer.html',1,'tiny_dlna']]],
+  ['dlnacontrolpointmediaserver_2',['DLNAControlPointMediaServer',['../classtiny__dlna_1_1DLNAControlPointMediaServer.html',1,'tiny_dlna']]],
+  ['dlnacontrolpointrequestparser_3',['DLNAControlPointRequestParser',['../classtiny__dlna_1_1DLNAControlPointRequestParser.html',1,'tiny_dlna']]],
+  ['dlnadevice_4',['DLNADevice',['../classtiny__dlna_1_1DLNADevice.html',1,'tiny_dlna']]],
+  ['dlnadeviceinfo_5',['DLNADeviceInfo',['../classtiny__dlna_1_1DLNADeviceInfo.html',1,'tiny_dlna']]],
+  ['dlnadevicerequestparser_6',['DLNADeviceRequestParser',['../classtiny__dlna_1_1DLNADeviceRequestParser.html',1,'tiny_dlna']]],
+  ['dlnamediarenderer_7',['DLNAMediaRenderer',['../classtiny__dlna_1_1DLNAMediaRenderer.html',1,'tiny_dlna']]],
+  ['dlnamediaserver_8',['DLNAMediaServer',['../classtiny__dlna_1_1DLNAMediaServer.html',1,'tiny_dlna']]],
+  ['dlnaserviceinfo_9',['DLNAServiceInfo',['../classtiny__dlna_1_1DLNAServiceInfo.html',1,'tiny_dlna']]]
 ];
--- search/files_1.js
+++ search/files_1.js
@@ -2,15 +2,14 @@ var searchData=
 [
   ['dlna_2eh_0',['DLNA.h',['../DLNA_8h.html',1,'']]],
   ['dlna_5fconfig_2eh_1',['dlna_config.h',['../dlna__config_8h.html',1,'']]],
-  ['dlnacontext_2eh_2',['DLNAContext.h',['../DLNAContext_8h.html',1,'']]],
-  ['dlnacontrolpoint_2eh_3',['DLNAControlPoint.h',['../DLNAControlPoint_8h.html',1,'']]],
-  ['dlnacontrolpointmediarenderer_2eh_4',['DLNAControlPointMediaRenderer.h',['../DLNAControlPointMediaRenderer_8h.html',1,'']]],
-  ['dlnacontrolpointmediaserver_2eh_5',['DLNAControlPointMediaServer.h',['../DLNAControlPointMediaServer_8h.html',1,'']]],
-  ['dlnacontrolpointrequestparser_2eh_6',['DLNAControlPointRequestParser.h',['../DLNAControlPointRequestParser_8h.html',1,'']]],
-  ['dlnadevice_2eh_7',['DLNADevice.h',['../DLNADevice_8h.html',1,'']]],
-  ['dlnadeviceinfo_2eh_8',['DLNADeviceInfo.h',['../DLNADeviceInfo_8h.html',1,'']]],
-  ['dlnadevicerequestparser_2eh_9',['DLNADeviceRequestParser.h',['../DLNADeviceRequestParser_8h.html',1,'']]],
-  ['dlnamediarenderer_2eh_10',['DLNAMediaRenderer.h',['../DLNAMediaRenderer_8h.html',1,'']]],
-  ['dlnamediaserver_2eh_11',['DLNAMediaServer.h',['../DLNAMediaServer_8h.html',1,'']]],
-  ['dlnaserviceinfo_2eh_12',['DLNAServiceInfo.h',['../DLNAServiceInfo_8h.html',1,'']]]
+  ['dlnacontrolpoint_2eh_2',['DLNAControlPoint.h',['../DLNAControlPoint_8h.html',1,'']]],
+  ['dlnacontrolpointmediarenderer_2eh_3',['DLNAControlPointMediaRenderer.h',['../DLNAControlPointMediaRenderer_8h.html',1,'']]],
+  ['dlnacontrolpointmediaserver_2eh_4',['DLNAControlPointMediaServer.h',['../DLNAControlPointMediaServer_8h.html',1,'']]],
+  ['dlnacontrolpointrequestparser_2eh_5',['DLNAControlPointRequestParser.h',['../DLNAControlPointRequestParser_8h.html',1,'']]],
+  ['dlnadevice_2eh_6',['DLNADevice.h',['../DLNADevice_8h.html',1,'']]],
+  ['dlnadeviceinfo_2eh_7',['DLNADeviceInfo.h',['../DLNADeviceInfo_8h.html',1,'']]],
+  ['dlnadevicerequestparser_2eh_8',['DLNADeviceRequestParser.h',['../DLNADeviceRequestParser_8h.html',1,'']]],
+  ['dlnamediarenderer_2eh_9',['DLNAMediaRenderer.h',['../DLNAMediaRenderer_8h.html',1,'']]],
+  ['dlnamediaserver_2eh_10',['DLNAMediaServer.h',['../DLNAMediaServer_8h.html',1,'']]],
+  ['dlnaserviceinfo_2eh_11',['DLNAServiceInfo.h',['../DLNAServiceInfo_8h.html',1,'']]]
 ];
