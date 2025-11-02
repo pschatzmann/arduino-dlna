1251f2572d97d180f7e2968d56f2dcf3880f26a2
pschatzmann <phil.schatzmann@gmail.com>
2025-11-02 06:17:11 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@9433e92e1e895e9b6ecd2e4afaa7726b153e0d24 🚀

--- search/functions_12.js
+++ search/functions_12.js
@@ -8,12 +8,13 @@ var searchData=
   ['updatereceived_5',['updateReceived',['../classtiny__dlna_1_1SubscriptionMgrControlPoint.html#a0d42d3bc8b23f1fbd021a1ab8fcec672',1,'tiny_dlna::SubscriptionMgrControlPoint']]],
   ['updatesubscriptions_6',['updateSubscriptions',['../classtiny__dlna_1_1SubscriptionMgrControlPoint.html#aca68cf7fbe6b981985deaf0f6285802a',1,'tiny_dlna::SubscriptionMgrControlPoint']]],
   ['updatetimestamp_7',['updateTimestamp',['../classtiny__dlna_1_1DLNADeviceInfo.html#a387ab3e1fce34f8add7fa402309340f4',1,'tiny_dlna::DLNADeviceInfo']]],
-  ['url_8',['url',['../classtiny__dlna_1_1Url.html#aad16241fe635aa19a84250fbfcf0fa71',1,'tiny_dlna::Url']]],
-  ['url_9',['Url',['../classtiny__dlna_1_1Url.html#a3827afe8ebae025faf0bdfdc59fcddda',1,'tiny_dlna::Url::Url(Url &amp;url)'],['../classtiny__dlna_1_1Url.html#abc4df2742d9fa125c161d5b61148acb1',1,'tiny_dlna::Url::Url(const char *url)'],['../classtiny__dlna_1_1Url.html#a3458de6d72b0ee2650e35ef3e78ad5f1',1,'tiny_dlna::Url::Url()=default']]],
-  ['urldecode_10',['urlDecode',['../classtiny__dlna_1_1Str.html#a5fc09f908226045d08a72f8c2a7deea1',1,'tiny_dlna::Str']]],
-  ['urldecode2_11',['urldecode2',['../classtiny__dlna_1_1HttpParameters.html#a455b5d18a103964ed2200512886da552',1,'tiny_dlna::HttpParameters']]],
-  ['urlencode_12',['urlEncode',['../classtiny__dlna_1_1Str.html#a215af0c16a6b48a8acc24c83018b7a0b',1,'tiny_dlna::Str']]],
-  ['urlencodechar_13',['urlEncodeChar',['../classtiny__dlna_1_1Str.html#ad9fb40aa7ad9171331f11b93ac281d7d',1,'tiny_dlna::Str']]],
-  ['urlpath_14',['urlPath',['../classtiny__dlna_1_1HttpHeader.html#a5e1aa7dd681442d42b0aeb0951f45165',1,'tiny_dlna::HttpHeader']]],
-  ['urlroot_15',['urlRoot',['../classtiny__dlna_1_1Url.html#a52c9750f97e0fedbde52fe3bf4282afb',1,'tiny_dlna::Url']]]
+  ['url_8',['Url',['../classtiny__dlna_1_1Url.html#a3458de6d72b0ee2650e35ef3e78ad5f1',1,'tiny_dlna::Url']]],
+  ['url_9',['url',['../classtiny__dlna_1_1Url.html#aad16241fe635aa19a84250fbfcf0fa71',1,'tiny_dlna::Url']]],
+  ['url_10',['Url',['../classtiny__dlna_1_1Url.html#a3827afe8ebae025faf0bdfdc59fcddda',1,'tiny_dlna::Url::Url(Url &amp;url)'],['../classtiny__dlna_1_1Url.html#abc4df2742d9fa125c161d5b61148acb1',1,'tiny_dlna::Url::Url(const char *url)']]],
+  ['urldecode_11',['urlDecode',['../classtiny__dlna_1_1Str.html#a5fc09f908226045d08a72f8c2a7deea1',1,'tiny_dlna::Str']]],
+  ['urldecode2_12',['urldecode2',['../classtiny__dlna_1_1HttpParameters.html#a455b5d18a103964ed2200512886da552',1,'tiny_dlna::HttpParameters']]],
+  ['urlencode_13',['urlEncode',['../classtiny__dlna_1_1Str.html#a215af0c16a6b48a8acc24c83018b7a0b',1,'tiny_dlna::Str']]],
+  ['urlencodechar_14',['urlEncodeChar',['../classtiny__dlna_1_1Str.html#ad9fb40aa7ad9171331f11b93ac281d7d',1,'tiny_dlna::Str']]],
+  ['urlpath_15',['urlPath',['../classtiny__dlna_1_1HttpHeader.html#a5e1aa7dd681442d42b0aeb0951f45165',1,'tiny_dlna::HttpHeader']]],
+  ['urlroot_16',['urlRoot',['../classtiny__dlna_1_1Url.html#a52c9750f97e0fedbde52fe3bf4282afb',1,'tiny_dlna::Url']]]
 ];
