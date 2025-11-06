fcdb48dd495e46f01c035691533a43ac5e4e8f01
pschatzmann <phil.schatzmann@gmail.com>
2025-11-06 06:32:37 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@f8c0e1ce74592224028c63f0cc0e6b317e58808d 🚀

--- search/variables_6.js
+++ search/variables_6.js
@@ -5,7 +5,8 @@ var searchData=
   ['g_5fstream_5fget_5fdata_5fcb_2',['g_stream_get_data_cb',['../classtiny__dlna_1_1DLNAMediaServer.html#a5b8bd9f8c62d8ab915db20063d17631a',1,'tiny_dlna::DLNAMediaServer']]],
   ['g_5fstream_5fnumberreturned_3',['g_stream_numberReturned',['../classtiny__dlna_1_1DLNAMediaServer.html#a196af62bf8cdc5fb391d584affef9f2a',1,'tiny_dlna::DLNAMediaServer']]],
   ['g_5fstream_5freference_4',['g_stream_reference',['../classtiny__dlna_1_1DLNAMediaServer.html#ac537a8fd0e29610e73f862b3d96c7b21',1,'tiny_dlna::DLNAMediaServer']]],
-  ['g_5fstream_5ftotalmatches_5',['g_stream_totalMatches',['../classtiny__dlna_1_1DLNAMediaServer.html#a4a5d21e70c76aeed0ec28cec96b91312',1,'tiny_dlna::DLNAMediaServer']]],
-  ['g_5fstream_5fupdateid_6',['g_stream_updateID',['../classtiny__dlna_1_1DLNAMediaServer.html#a377aa16982cde12a28480d22246bc3cc',1,'tiny_dlna::DLNAMediaServer']]],
-  ['get_5fdata_5fcb_7',['get_data_cb',['../classtiny__dlna_1_1DLNAMediaServer.html#a995dab19e18e3d63d8a0724353575c1f',1,'tiny_dlna::DLNAMediaServer']]]
+  ['g_5fstream_5fstartingindex_5',['g_stream_startingIndex',['../classtiny__dlna_1_1DLNAMediaServer.html#a1c5ea5c2d01fe9165bd00dcfa01f82c0',1,'tiny_dlna::DLNAMediaServer']]],
+  ['g_5fstream_5ftotalmatches_6',['g_stream_totalMatches',['../classtiny__dlna_1_1DLNAMediaServer.html#a4a5d21e70c76aeed0ec28cec96b91312',1,'tiny_dlna::DLNAMediaServer']]],
+  ['g_5fstream_5fupdateid_7',['g_stream_updateID',['../classtiny__dlna_1_1DLNAMediaServer.html#a377aa16982cde12a28480d22246bc3cc',1,'tiny_dlna::DLNAMediaServer']]],
+  ['get_5fdata_5fcb_8',['get_data_cb',['../classtiny__dlna_1_1DLNAMediaServer.html#a995dab19e18e3d63d8a0724353575c1f',1,'tiny_dlna::DLNAMediaServer']]]
 ];
