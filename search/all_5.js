bc310dc68ad006188b168e6a84869728401c2348
pschatzmann <phil.schatzmann@gmail.com>
2025-11-08 22:12:20 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@2aca2ad2709bf4a38835ecd351cee5876fba3afa 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,13 +1,12 @@
 var searchData=
 [
-  ['finalize_0',['finalize',['../classtiny__dlna_1_1XMLDeviceParser.html#a99ed4babb56b05a6a118b27d5eddde0f',1,'tiny_dlna::XMLDeviceParser']]],
-  ['findargument_1',['findArgument',['../classtiny__dlna_1_1ActionReply.html#a0ffbfd07ecaee32284e30841f3050fd9',1,'tiny_dlna::ActionReply']]],
-  ['findgt_2',['findGt',['../classtiny__dlna_1_1XMLParser.html#ac1d60d06f67666127d4710a8055af23f',1,'tiny_dlna::XMLParser']]],
-  ['first_3',['first',['../classtiny__dlna_1_1List.html#a252c9f78f2ec71ad78897f4908f7e590',1,'tiny_dlna::List']]],
-  ['firstdatanode_4',['firstDataNode',['../classtiny__dlna_1_1List.html#add925c2c04f7e0881e35473c8ad17022',1,'tiny_dlna::List']]],
-  ['floattostring_5',['floatToString',['../classtiny__dlna_1_1StrView.html#af5f53e9a6b2a8ed4400d55a074d91cbe',1,'tiny_dlna::StrView']]],
-  ['fn_6',['fn',['../classtiny__dlna_1_1HttpRequestHandlerLine.html#a5667a381065eb867e1694f07fb65d32f',1,'tiny_dlna::HttpRequestHandlerLine']]],
-  ['folder_7',['Folder',['../namespacetiny__dlna.html#a85d3f686372782f654ff23302800aac9ab0f2b97dc5d2b76b26e040408bb1d8af',1,'tiny_dlna']]],
-  ['friendly_5fname_8',['friendly_name',['../classtiny__dlna_1_1DLNADeviceInfo.html#aee4058260a894380743eace676d9f0a7',1,'tiny_dlna::DLNADeviceInfo']]],
-  ['from_9',['from',['../classtiny__dlna_1_1HttpRequestRewrite.html#ab207c9b3cde4010ce6df5118cbee6bea',1,'tiny_dlna::HttpRequestRewrite']]]
+  ['findargument_0',['findArgument',['../classtiny__dlna_1_1ActionReply.html#a0ffbfd07ecaee32284e30841f3050fd9',1,'tiny_dlna::ActionReply']]],
+  ['findgt_1',['findGt',['../classtiny__dlna_1_1XMLParser.html#ac1d60d06f67666127d4710a8055af23f',1,'tiny_dlna::XMLParser']]],
+  ['first_2',['first',['../classtiny__dlna_1_1List.html#a252c9f78f2ec71ad78897f4908f7e590',1,'tiny_dlna::List']]],
+  ['firstdatanode_3',['firstDataNode',['../classtiny__dlna_1_1List.html#add925c2c04f7e0881e35473c8ad17022',1,'tiny_dlna::List']]],
+  ['floattostring_4',['floatToString',['../classtiny__dlna_1_1StrView.html#af5f53e9a6b2a8ed4400d55a074d91cbe',1,'tiny_dlna::StrView']]],
+  ['fn_5',['fn',['../classtiny__dlna_1_1HttpRequestHandlerLine.html#a5667a381065eb867e1694f07fb65d32f',1,'tiny_dlna::HttpRequestHandlerLine']]],
+  ['folder_6',['Folder',['../namespacetiny__dlna.html#a85d3f686372782f654ff23302800aac9ab0f2b97dc5d2b76b26e040408bb1d8af',1,'tiny_dlna']]],
+  ['friendly_5fname_7',['friendly_name',['../classtiny__dlna_1_1DLNADeviceInfo.html#aee4058260a894380743eace676d9f0a7',1,'tiny_dlna::DLNADeviceInfo']]],
+  ['from_8',['from',['../classtiny__dlna_1_1HttpRequestRewrite.html#ab207c9b3cde4010ce6df5118cbee6bea',1,'tiny_dlna::HttpRequestRewrite']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['finalize_0',['finalize',['../classtiny__dlna_1_1XMLDeviceParser.html#a99ed4babb56b05a6a118b27d5eddde0f',1,'tiny_dlna::XMLDeviceParser']]],
-  ['findargument_1',['findArgument',['../classtiny__dlna_1_1ActionReply.html#a0ffbfd07ecaee32284e30841f3050fd9',1,'tiny_dlna::ActionReply']]],
-  ['findgt_2',['findGt',['../classtiny__dlna_1_1XMLParser.html#ac1d60d06f67666127d4710a8055af23f',1,'tiny_dlna::XMLParser']]],
-  ['firstdatanode_3',['firstDataNode',['../classtiny__dlna_1_1List.html#add925c2c04f7e0881e35473c8ad17022',1,'tiny_dlna::List']]],
-  ['floattostring_4',['floatToString',['../classtiny__dlna_1_1StrView.html#af5f53e9a6b2a8ed4400d55a074d91cbe',1,'tiny_dlna::StrView']]]
+  ['findargument_0',['findArgument',['../classtiny__dlna_1_1ActionReply.html#a0ffbfd07ecaee32284e30841f3050fd9',1,'tiny_dlna::ActionReply']]],
+  ['findgt_1',['findGt',['../classtiny__dlna_1_1XMLParser.html#ac1d60d06f67666127d4710a8055af23f',1,'tiny_dlna::XMLParser']]],
+  ['firstdatanode_2',['firstDataNode',['../classtiny__dlna_1_1List.html#add925c2c04f7e0881e35473c8ad17022',1,'tiny_dlna::List']]],
+  ['floattostring_3',['floatToString',['../classtiny__dlna_1_1StrView.html#af5f53e9a6b2a8ed4400d55a074d91cbe',1,'tiny_dlna::StrView']]]
 ];
