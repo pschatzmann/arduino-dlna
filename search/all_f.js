1263d45052b5cbe277fddcb55d67c04a66bdb487
pschatzmann <phil.schatzmann@gmail.com>
2025-11-22 10:25:46 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@d4dfb74b13877bcbd163a2c68b8cdfa2b6d6416b 🚀

--- search/all_f.js
+++ search/all_f.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['querytype_5f_0',['queryType_',['../classtiny__dlna_1_1SdFatContentProvider.html#aaa35e2d439b50d96c92067f0684409b9',1,'tiny_dlna::SdFatContentProvider']]],
-  ['queue_1',['queue',['../classtiny__dlna_1_1Scheduler.html#abc0f361ef0885ee7a345e8b1c6ecfcb7',1,'tiny_dlna::Scheduler::queue()'],['../classtiny__dlna_1_1UDPAsyncService.html#a8ad6c88ed27283e09f6457bbd03e1c49',1,'tiny_dlna::UDPAsyncService::queue()']]],
-  ['queue_2',['Queue',['../classtiny__dlna_1_1Queue.html#adbecdd81d187d9a88dfc79ddd7f9dd03',1,'tiny_dlna::Queue::Queue()=default'],['../classtiny__dlna_1_1Queue.html#ab07499a144953a7eef2cc2958596fb41',1,'tiny_dlna::Queue::Queue(const Alloc &amp;alloc)'],['../classtiny__dlna_1_1Queue.html',1,'tiny_dlna::Queue&lt; T, Alloc &gt;']]],
-  ['queue_2eh_3',['Queue.h',['../Queue_8h.html',1,'']]],
-  ['queue_3c_20tiny_5fdlna_3a_3arequestdata_20_3e_4',['Queue&lt; tiny_dlna::RequestData &gt;',['../classtiny__dlna_1_1Queue.html',1,'tiny_dlna']]],
-  ['queuelockfree_5',['QueueLockFree',['../classtiny__dlna_1_1QueueLockFree.html#ae820f568fdb673ca3feb27625c947a69',1,'tiny_dlna::QueueLockFree::QueueLockFree()'],['../classtiny__dlna_1_1QueueLockFree.html',1,'tiny_dlna::QueueLockFree&lt; T &gt;']]],
-  ['queuelockfree_2eh_6',['QueueLockFree.h',['../QueueLockFree_8h.html',1,'']]]
+  ['queue_1',['Queue',['../classtiny__dlna_1_1Queue.html#adbecdd81d187d9a88dfc79ddd7f9dd03',1,'tiny_dlna::Queue::Queue()=default'],['../classtiny__dlna_1_1Queue.html#ab07499a144953a7eef2cc2958596fb41',1,'tiny_dlna::Queue::Queue(const Alloc &amp;alloc)']]],
+  ['queue_2',['queue',['../classtiny__dlna_1_1Scheduler.html#abc0f361ef0885ee7a345e8b1c6ecfcb7',1,'tiny_dlna::Scheduler::queue()'],['../classtiny__dlna_1_1UDPAsyncService.html#a8ad6c88ed27283e09f6457bbd03e1c49',1,'tiny_dlna::UDPAsyncService::queue()']]],
+  ['queue_3',['Queue',['../classtiny__dlna_1_1Queue.html',1,'tiny_dlna']]],
+  ['queue_2eh_4',['Queue.h',['../Queue_8h.html',1,'']]],
+  ['queue_3c_20tiny_5fdlna_3a_3arequestdata_20_3e_5',['Queue&lt; tiny_dlna::RequestData &gt;',['../classtiny__dlna_1_1Queue.html',1,'tiny_dlna']]],
+  ['queuelockfree_6',['QueueLockFree',['../classtiny__dlna_1_1QueueLockFree.html#ae820f568fdb673ca3feb27625c947a69',1,'tiny_dlna::QueueLockFree::QueueLockFree()'],['../classtiny__dlna_1_1QueueLockFree.html',1,'tiny_dlna::QueueLockFree&lt; T &gt;']]],
+  ['queuelockfree_2eh_7',['QueueLockFree.h',['../QueueLockFree_8h.html',1,'']]]
 ];
