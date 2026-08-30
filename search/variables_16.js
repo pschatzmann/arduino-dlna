554ef373bd300d276ed7f37fbc7a2f3ee67edf74
pschatzmann <phil.schatzmann@gmail.com>
2026-08-30 14:07:22 +0000

    Deploying to doxygen from @ pschatzmann/arduino-dlna@d01731735a56c2ebd6a2104b5105b1065e85f02b 🚀

--- search/variables_16.js
+++ search/variables_16.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['xhandle_0',['xHandle',['../classtiny__dlna_1_1TaskRTOS.html#a174802ebe6b4163957051414c9e9c352',1,'tiny_dlna::TaskRTOS']]],
-  ['xml_1',['xml',['../classtiny__dlna_1_1NotifyReplyCP.html#a863f13e8c11a084051e6b07c2abf15bb',1,'tiny_dlna::NotifyReplyCP']]],
-  ['xml_5fparser_2',['xml_parser',['../classtiny__dlna_1_1XMLDeviceParser.html#a7ba4af2d134f9c0f26e35a71b52c90d0',1,'tiny_dlna::XMLDeviceParser']]],
-  ['xml_5fprinter_3',['xml_printer',['../classtiny__dlna_1_1DLNAControlPoint.html#adb01026d000c84945a2fe223a68821c8',1,'tiny_dlna::DLNAControlPoint']]]
+  ['x_5fdlnacap_0',['x_dlnacap',['../classtiny__dlna_1_1DLNADeviceInfo.html#a33d75c2f7450634c67e5c7f3f88802f0',1,'tiny_dlna::DLNADeviceInfo']]],
+  ['xhandle_1',['xHandle',['../classtiny__dlna_1_1TaskRTOS.html#a174802ebe6b4163957051414c9e9c352',1,'tiny_dlna::TaskRTOS']]],
+  ['xml_2',['xml',['../classtiny__dlna_1_1NotifyReplyCP.html#a863f13e8c11a084051e6b07c2abf15bb',1,'tiny_dlna::NotifyReplyCP']]],
+  ['xml_5fparser_3',['xml_parser',['../classtiny__dlna_1_1XMLDeviceParser.html#a7ba4af2d134f9c0f26e35a71b52c90d0',1,'tiny_dlna::XMLDeviceParser']]],
+  ['xml_5fprinter_4',['xml_printer',['../classtiny__dlna_1_1DLNAControlPoint.html#adb01026d000c84945a2fe223a68821c8',1,'tiny_dlna::DLNAControlPoint']]]
 ];
